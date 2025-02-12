98b37cbcf1afdea7fc9119492cbf34e0bddefd5e
Alexander Jochum <darkcloud14@gmail.com>
2025-02-12 11:02:24 +0100

    Initial commit

---
+++ ReactiveUI_SourceGen_ObservableAsProperty_Issue/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+
+namespace ReactiveUI_SourceGen_ObservableAsProperty_Issue;
+
+public partial class TestClass : ReactiveObject
+{
+    [ObservableAsProperty]
+    private bool _observableTestField;
+
+    public TestClass()
+    {
+    }
+
+    [ObservableAsProperty]
+    public partial bool ObservableTestProperty { get; }
+}
